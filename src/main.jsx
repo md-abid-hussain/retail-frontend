e8279d95300b80d85dc585d19aa3bc757714aa29
Md Abid Hussain <abid0hussain1733@gmail.com>
2024-01-15 21:51:26 +0530

    configure routing and store

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { disableReactDevTools } from "@fvilers/disable-react-devtools";
+import { store } from "./app/store";
+import { Provider } from "react-redux";
 
 if (import.meta.env.PROD) {
   disableReactDevTools();
@@ -11,10 +13,12 @@ if (import.meta.env.PROD) {
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/*" element={<App />} />
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/*" element={<App />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
